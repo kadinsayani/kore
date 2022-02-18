3542e05ae3430b9cbbb946a5675d08ec0ab7c38e
Kadin Sayani <kadinsayani8@gmail.com>
2022-02-18 15:35:26 -0700

    Initial Commit

---
+++ Kore/KoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoreApp.swift
+//  Kore
+//
+//  Created by Kadin Sayani on 2022-02-18.
+//
+
+import SwiftUI
+
+@main
+struct KoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

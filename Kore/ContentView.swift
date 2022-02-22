e1363487643ff6bb5972c038e06f1657618a666f
Kadin Sayani <kadinsayani8@gmail.com>
2022-02-22 15:38:41 -0700

    added firebase configuration and packages

--- Kore/ContentView.swift
+++ Kore/ContentView.swift
@@ -6,12 +6,37 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    print("Application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 struct ContentView: View {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var user: String = "User"
+    var program: String = "Workout 1"
+    var daysOnProgram: Int = 5
+    var programDays: Int = 30
+    
     var body: some View {
         NavigationView() {
             TabView {
-                Text("Dashboard")
+                VStack {
+                    Text("Welcome, \(user)!")
+                        .font(.system(size: 32, weight: .bold, design: .rounded))
+                    Spacer()
+                    VStack {
+                        Text("Current Program: \(program)")
+                        Text("Day \(daysOnProgram) out of \(programDays)")
+                    }
+                }
                     .tabItem {
                         Image(systemName: "house")
                         Text("Dashboard")
@@ -23,7 +48,7 @@ struct ContentView: View {
                     }
                 List(1...10, id: \.self) { index in
                     NavigationLink(
-                        destination: Text("Workout #\(index) Details"), label: {
+                        destination: Text("workout"), label: {
                             Text("Workout #\(index)").font(.system(size: 20, weight: .bold, design: .rounded))
                         })
                 }
